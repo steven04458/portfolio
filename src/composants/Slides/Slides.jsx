67de91d6314e9902c319d5d3ae80f92a11c9a67c
steven04458 <steven.spein2@gmail.com>
2024-09-19 17:45:12 +0200

    changement slide + corrective

--- src/composants/Slides/Slides.jsx
+++ src/composants/Slides/Slides.jsx
@@ -1,31 +1,87 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 import $ from 'jquery';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel';
 import './Slides.css';
 
-function Slide({ id, bgClass, seanceClass, titreClass, listClass }) {
+function Slide() {
+  const sliderForRef = useRef(null);
+  const sliderNavRef = useRef(null);
+
+  useEffect(() => {
+    if (sliderForRef.current && sliderNavRef.current) {
+      $(sliderForRef.current).slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: sliderNavRef.current
+      });
+      
+      $(sliderNavRef.current).slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: sliderForRef.current,
+        dots: true,
+        focusOnSelect: true,
+        responsive: [
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+          {
+            breakpoint: 960,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 1270,
+            settings: {
+              slidesToShow: 3,
+            }
+          }
+        ]
+      });
+    }
+    
+
+    return () => {
+      // Cleanup on component unmount
+      $(sliderForRef.current).slick('unslick');
+      $(sliderNavRef.current).slick('unslick');
+    };
+  }, []);
+
   return (
-    <div className="swiper-slide">
-      <a href={`#${id}`} id={`link${id}`} className="link" rel={id}>
-        <div className={`swiper-card ${bgClass}`}>
-          <p className={`seance ${seanceClass}`}>
-            <span className="fr_lang"></span>
-            <span className="en_lang"></span>
-          </p>
-          <p className={`titre-seance ${titreClass}`}>
-            <span className="fr_lang"></span>
-            <span className="en_lang"></span>
-          </p>
-          <p className={`list ${listClass}`}>
-            <span className="fr_lang"></span>
-            <span className="en_lang"></span>
-          </p>
-          <br /><br />
-          <svg id="arrow-down" x="0px" y="0px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 220 220">
-            <path d="M100,157.2c-4.2,0-8.4-1.6-11.6-4.7l-83.3-81c-6.6-6.4-6.8-17-0.3-23.6c6.4-6.6,17-6.8,23.6-0.3l71.7,69.7l71.7-69.7 c6.6-6.4,17.2-6.3,23.6,0.3c6.4,6.6,6.3,17.2-0.3,23.6l-83.3,81C108.4,155.6,104.2,157.2,100,157.2z"/>
-          </svg>
+    <div className="main">
+      <div className="slider slider-nav" ref={sliderNavRef}>
+        <div>
+          <p>janv.2021 - mars 2021</p>
+          <h3>Technicien Support Informatique - Stage</h3>
         </div>
-      </a>
+        <div>
+          <p>janv.2022 - mai 2022</p>
+          <h3>Technicien Support Informatique</h3>
+        </div>
+        <div>
+          <p></p>
+          <h3>3</h3>
+        </div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+      </div>
+      <div className="slider slider-for" ref={sliderForRef}>
+        <div><h3>Technicien Support Informatique - Stage</h3></div>
+        <div><h3>Technicien Support Informatique</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+      </div>
     </div>
   );
 }
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import $ from 'jquery';
 import Swiper from 'swiper';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './Blog.css';
 import Header from '../../composants/Header/Header.jsx';
@@ -10,101 +13,6 @@ import Slide from '../../composants/Slides/Slides.jsx';
 
 function Blog() {
   const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    // SLIDER SWIPER  
-    $(document).ready(function () {
-      var mySwiper = new Swiper('.swiper-container', {
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        },
-        slidesPerView: 1,
-        spaceBetween: 10,
-        breakpoints: {
-          960: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          1270: {
-            slidesPerView: 3,
-            spaceBetween: 10,
-          },
-        },
-      });
-    });
-
-    // LIEN HREF TABLEAU
-    $('tr[data-href]').each(function() {
-      var $th = $(this);
-      $th.on('touchstart mousedown', function() {
-        window.open($th.attr('data-href'), $th.attr('data-target'));
-      });
-    });
-    $('td[data-href]').each(function() {
-      var $th = $(this);
-      $th.on('click', function() {
-        return !window.open($th.attr('data-href'));
-      });
-    });
-
-    // OPEN & CLOSE POPUP
-    $('.cd-popup').on('touchstart touchmove touchend mousedown', function(event) {
-      if ($(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup')) {
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).removeClass('is-visible');
-      }
-    });
-
-    // Close popup when clicking the esc keyboard button
-    $(document).keyup(function(event) {
-      if (event.which == '27') {
-        $('.cd-popup').removeClass('is-visible');
-      }
-    });
-
-    // RÉSUMÉS LONGS
-    $(".fold-table tr.view").on("click", function() {
-      $(this).toggleClass("open").prev(".fold").toggleClass("open");
-    });
-
-    // ANNEXE
-    var $allContentDivs = $('#infocontent div').hide(); // Hide All Content Divs
-    $('#linkwrapper a').click(function() {
-      var $contentDiv = $("#" + this.id + "content");
-
-      if ($contentDiv.is(":visible")) {
-        $contentDiv.hide(); // Hide Div
-      } else {
-        $allContentDivs.hide(); // Hide All Divs
-        $contentDiv.fadeIn('slow'); // Show Div 
-        $contentDiv.get(0).scrollIntoView();
-      }
-
-      // SCROLLABLE 
-      $('.scrollable').css('display', 'block');
-      return false;
-    });
-
-    $('#prev-next-program a').click(function() {
-      var $contentDiv = $("#" + this.id + "content");
-
-      if ($contentDiv.is(":visible")) {
-        $contentDiv.hide(); // Hide Div
-      } else {
-        $allContentDivs.hide(); // Hide All Divs
-        $contentDiv.fadeIn('slow'); // Show Div 
-        $contentDiv.get(0).scrollIntoView();
-      }
-
-      // SCROLLABLE 
-      $('.scrollable').css('display', 'block');
-      return false;
-    });
-  }, []);
 
   return (
     <>
@@ -117,28 +25,7 @@ function Blog() {
         </section>        
       </header>
       <BoutonsToTheTop /> 
-      <div className='contenant'>
-        <div className="swiper-main">
-          <div className="swiper-button-prev">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 220 220" transform="scale(1 -1)rotate(90)"><path d="M100 157.2c-4.2 0-8.4-1.6-11.6-4.7l-83.3-81c-6.6-6.4-6.8-17-.3-23.6 6.4-6.6 17-6.8 23.6-.3l71.7 69.7 71.7-69.7c6.6-6.4 17.2-6.3 23.6.3s6.3 17.2-.3 23.6l-83.3 81c-3.4 3.1-7.6 4.7-11.8 4.7"/></svg>            <br />
-            <span className="fr_lang button-txt">précédent</span>
-          </div> 
-          
-          <div className="swiper-container">
-            <div className="swiper-wrapper" id="linkwrapper">
-              <Slide id="seance1" bgClass="bgcard1" seanceClass="seance-semaine1" titreClass="titre-seance1" listClass="list" />
-              <Slide id="seance2" bgClass="bgcard2" seanceClass="seance-semaine1" titreClass="titre-seance2" listClass="list" />
-              <Slide id="seance3" bgClass="bgcard3" seanceClass="seance-semaine1" titreClass="titre-seance3" listClass="list" />
-              <Slide id="seance4" bgClass="bgcard4" seanceClass="seance-semaine1" titreClass="titre-seance4" listClass="list" />
-              {/* Add more slides as needed */}
-            </div>
-          </div>
-          <div className="swiper-button-next">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 220 220" transform="matrix(0 1 1 0 0 0)"><path d="M100 157.2c-4.2 0-8.4-1.6-11.6-4.7l-83.3-81c-6.6-6.4-6.8-17-.3-23.6 6.4-6.6 17-6.8 23.6-.3l71.7 69.7 71.7-69.7c6.6-6.4 17.2-6.3 23.6.3s6.3 17.2-.3 23.6l-83.3 81c-3.4 3.1-7.6 4.7-11.8 4.7"/></svg>
-            <span className="fr_lang button-txt">suivant</span>
-          </div>
-        </div>
-      </div>
+      <Slide />
       <Footer />
     </>
   );
